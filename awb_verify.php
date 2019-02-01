5b00dd4e5222ce66931d80b24d94a353a0c60f7b
Oajdea Alin <alin.oajdea@gmail.com>
2019-02-01 18:43:24 +0200

    Integrate the barcode in the order verification

--- awb_verify.php
+++ awb_verify.php
@@ -1,15 +1,17 @@
 <?php
 include_once('config.php');
 include_once('template_parts/header.php');
-
+include_once('functions.php');
 ?>
 
 <?php
-$awb = $_POST['awb'];
+$awb = $_GET['awb'];
 $query = "SELECT * FROM awb WHERE awb='$awb'";
 echo "<h1><b> <center>Order Details</center> </b></h1> <br> <br>";
  
 if ($result = $conn->query($query)) {
+
+  if($result->num_rows > 0){
  
     while ($row = $result->fetch_assoc()) {
         $field1name = $row["nume_prenume_expeditor"];
@@ -17,8 +19,8 @@ if ($result = $conn->query($query)) {
         $field3name = $row["detalii_expeditor"];
         $field4name = $row["detalii_destinatar"];
         $field5name = $row["awb"];
-        $field6name = $row["detalii"];
-        $field7name = $row["data"];
+        $field6name = $row["data"];
+        $field7name = $row["status"];
     }
  
     echo '<main role="main" class="container">';
@@ -35,18 +37,21 @@ if ($result = $conn->query($query)) {
       echo '<li class="list-group-item list-group-item-info">'.$field4name.'</li>';
     echo '<list-group-item list-group-item-primary>Tracking Number</li>';
       echo '<li class="list-group-item list-group-item-info">'.$field5name.'</li>';
-    echo '<list-group-item list-group-item-primary>Other Details</li>';
-      echo '<li class="list-group-item list-group-item-info">'.$field6name.'</li>';
     echo '<list-group-item list-group-item-primary>Date</li>';
+      echo '<li class="list-group-item list-group-item-info">'.$field6name.'</li>';
+    echo '<list-group-item list-group-item-primary>Status</li>';
     echo '<li class="list-group-item list-group-item-info">'.$field7name.'</li>';
+    echo '<list-group-item list-group-item-primary>Barcode</li>';
+    echo '<li class="list-group-item list-group-item-info">'.generare_cod_bare().'</li>';
     echo '</ul>';
     
     echo '</main>';
 
-/*freeresultset*/
-$result->free();
-}else{
-  echo'<h1>Order not found</h1>';
+
+  }
+  else{
+    echo'<h1>Order not found</h1>';
+  }
 }
 ?>
 <center><a class="btn btn-primary" href="/verify.php" role="button">Verify another</a></center>
